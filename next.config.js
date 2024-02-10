b124b516af46d48b1f2dd399116163739beb00be
karimGhost <74273577+karimGhost@users.noreply.github.com>
2024-02-10 13:42:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-    distDir: 'dist',
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+output: 'export',
 }
- 
-module.exports = nextConfig
 
+module.exports = nextConfig
+ 
