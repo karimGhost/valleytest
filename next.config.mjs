f324bcfc4726d493a3e7c86ee621b081704f609e
karimGhost <74273577+karimGhost@users.noreply.github.com>
2024-02-10 18:09:39 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-  
-   // Add basePath
-    basePath: '/valleytest'
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 };
 
 export default nextConfig;
