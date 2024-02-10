681aaaa63e0a6405f611c33fc1171353fac8fa56
Karim <abdulkarimkassimsalim@gmail.com>
2024-02-10 16:28:02 +0300

    Your commit message here

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: '/testvalley',
+    output: 'export',
+    distDir: 'out',
+
+};
 
 export default nextConfig;
